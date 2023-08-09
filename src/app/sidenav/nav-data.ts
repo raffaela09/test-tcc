cdeb55b3f4b26fc51a652de7f1bf5ef628b34e24
raffaela09 <126827785+raffaela09@users.noreply.github.com>
2023-08-09 13:59:54 -0300

    Add files via upload

---
+++ src/app/sidenav/nav-data.ts
@@ -0,0 +1,37 @@
+export const navbarData = [
+     {
+        routerlink: 'dashboard',
+        icon: 'bx bx-home-alt-2',
+        label: 'Dashboard'
+     },
+     {
+        routerlink: 'products',
+        icon: 'bx bx-shopping-bag',
+        label: 'Products'
+     },
+     {
+        routerlink: 'statistics',
+        icon: 'bx bx-bar-chart-alt',
+        label: 'Statistics'
+     },
+     {
+        routerlink: 'coupens',
+        icon: 'bx bx-tag',
+        label: 'Coupens'
+     },
+     {
+        routerlink: 'pages',
+        icon: 'bx bx-file',
+        label: 'Pages'
+     },
+     {
+        routerlink: 'media',
+        icon: 'bx bx-plus',
+        label: 'Media'
+     },
+     {
+        routerlink: 'settings',
+        icon: 'bx bx-cog',
+        label: 'Settings'
+     },
+];
